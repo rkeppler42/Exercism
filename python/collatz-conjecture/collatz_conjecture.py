2ba41de36e001b5f3e4f478a3932c7741d8824dd
Roberto Keppler <roberto.keppler@hotmail.com>
2024-05-27 15:26:42 -0300

    Second iteration of the Collatz Conjecture exercise

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,12 +1,24 @@
-def steps(number):
+"""
+Function to calculate the number of steps taken to get to number 1 through Collatz Conjecture
+"""
+
+def steps(number: int) -> int:
+    """
+
+    :param number: int - initial number to go through Collatz Conjecture.
+    :return: int - numbers of steps taken to get to number 1 through Collatz Conjecture.
+    """
 
     if number <= 0:
         raise ValueError("Only positive integers are allowed")
-    step = 0
+
+    step: int = 0
+
     while number != 1:
         if number % 2 == 0:
             number /= 2
         else:
             number = number * 3 + 1
         step += 1
+
     return step
